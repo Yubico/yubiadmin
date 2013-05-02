da4f4af69ae6ec1f44df8690859007b687827d09
Dain Nilsson <dain@yubico.com>
2013-05-02 13:52:58 +0200

    Bumped version post release.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from release import release
 
 setup(
     name='yubiadmin',
-    version='0.0.6',
+    version='0.0.7',
     author='Dain Nilsson',
     author_email='dain@yubico.com',
     maintainer='Yubico Open Source Maintainers',
