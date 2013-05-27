1e15bcba4ddca46ededafb5b2fe71381d7c7b107
Dain Nilsson <dain@yubico.com>
2013-05-27 14:00:01 +0200

    Added FreeRADIUS daemon status and controls.

--- yubiadmin/apps/freerad.py
+++ yubiadmin/apps/freerad.py
@@ -26,9 +26,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 from yubiadmin.util.app import App, render
-from yubiadmin.util.system import run
+from yubiadmin.util.system import run, invoke_rc_d
 from wtforms import Form
-from wtforms.fields import TextField, PasswordField
+from wtforms.fields import TextField
 import os
 
 __all__ = [
@@ -38,10 +38,19 @@ __all__ = [
 CLIENTS_CONFIG_FILE = '/etc/freeradius/clients.conf'
 
 
+def is_freerad_running():
+    status, _ = run('cat /var/run/freeradius/freeradius.pid | xargs kill -0')
+    return status == 0
+
+
 class RadTestForm(Form):
     legend = 'RADIUS test'
+    description = """
+    This utility allows you to test authentication against the RADIUS server
+    using the credentials entered below.
+    """
     username = TextField('Username')
-    password = PasswordField('Password')
+    password = TextField('Password')
 
 
 class FreeRadius(App):
@@ -63,12 +72,17 @@ class FreeRadius(App):
         General
         """
         alerts = []
+        form = RadTestForm()
+
         if 'username' in request.params:
-            username = request.params['username']
-            password = request.params.get('password', '')
+            form.process(request.params)
+            username = form.username.data
+            password = form.password.data
             cmd = 'radtest %s %s localhost 0 testing123' % (username, password)
             status, output = run(cmd)
-            alert = {'title': cmd, 'message': '<pre>%s</pre>' % output}
+            alert = {'title': 'Command: %s' % cmd}
+            alert['message'] = '<pre style="white-space: pre-wrap;">%s</pre>' \
+                % output
             if status == 0:
                 alert['type'] = 'success'
             elif status == 1:
@@ -79,12 +93,25 @@ class FreeRadius(App):
                     'Exit code: %d' % status
             alerts.append(alert)
 
-        return render('freerad/general', form=RadTestForm(), alerts=alerts)
+        return render('freerad/general', form=form, alerts=alerts,
+                      running=is_freerad_running())
 
     def clients(self, request):
         """
         RADIUS clients
         """
         return ''
+
+    def server(self, request):
+        if request.params['server'] == 'toggle':
+            if is_freerad_running():
+                invoke_rc_d('freeradius', 'stop')
+            else:
+                invoke_rc_d('freeradius', 'start')
+        else:
+            invoke_rc_d('freeradius', 'restart')
+
+        return self.redirect('/%s/general' % self.name)
+
 
 app = FreeRadius()
